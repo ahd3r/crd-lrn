c0a8c60ea7ca5a21acd576836edd6382234728ac
Andy <ander11110@gmail.com>
2025-11-06 06:45:53 +0000

    finished msg crd, hooray

--- message_crd/README.md
+++ message_crd/README.md
@@ -3,9 +3,9 @@
 To run things up:
 ```sh
 # configure k8s context and use it
-kubectl apply -f ./message_crd/crd.yaml
-sh ./message_crd/controller/deploy.sh
-kubectl logs deployment/msg-controller-dep --all-pods=true -f
+kubectl apply -f ./message_crd/crd.yaml # creates CRD within cluster
+sh ./message_crd/controller/deploy.sh # deploy custom controller into cluster (will work only if runs within the cluster)
+kubectl logs deployment/msg-controller-dep --all-pods=true -f # watch logs of the custom controller
 # open another terminal
-kubectl apply -f ./message_crd/message.yaml
+kubectl apply -f ./message_crd/message.yaml # create a resource that trigger controller
 ```
--- message_crd/controller/main.go
+++ message_crd/controller/main.go
@@ -16,20 +16,20 @@ import (
 )
 
 type Message struct { // type name should be same as Kind name of CRD
-	metav1.TypeMeta   `json:",inline"` // consist of kind and apiVersion, inline
-	metav1.ObjectMeta `json:"metadata,omitempty"` // omitempty
+	metav1.TypeMeta   `json:",inline"` // consist of kind and apiVersion
+	metav1.ObjectMeta `json:"metadata,omitempty"` // consist of all possible fields in metadata
 	Spec              struct {
 		Text string `json:"text"`
 	} `json:"spec"`
 }
 
-func (msg *Message) DeepCopyObject() runtime.Object {
+func (msg *Message) DeepCopyObject() runtime.Object { // required method in order to add to known types (scheme.AddKnownTypes)
 	out := new(Message)
 	*out = *msg
 	return out
 }
 
-type MessageList struct {
+type MessageList struct { // extra struct in addition to the above one in order to add new type of resource to cluster
     metav1.TypeMeta `json:",inline"`
     metav1.ListMeta `json:"metadata,omitempty"`
     Items           []Message `json:"items"`
@@ -41,26 +41,26 @@ func (msg *MessageList) DeepCopyObject() runtime.Object {
     return out
 }
 
-type MessageReconciler struct {
+type MessageReconciler struct { // reconcile initialization
 	client.Client
 }
 
 var count = 0
-func (r *MessageReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	logger := l.FromContext(ctx)
+func (r *MessageReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) { // the exact fuction gets to call on resource change
+	logger := l.FromContext(ctx) // build in logger can not be used
 	logger.Info("Reconcile is triggered")
 	var msg Message
-    if err := r.Get(ctx, req.NamespacedName, &msg); err != nil {
-		if errors.IsNotFound(err) {
+    if err := r.Get(ctx, req.NamespacedName, &msg); err != nil { // get resource that triggers reconcile
+		if errors.IsNotFound(err) { // avoid error on deletion, since delete also triggers reconcile and no resource is found on execution
 			return ctrl.Result{}, nil
 		}
-        return ctrl.Result{}, err
+        return ctrl.Result{}, err // if reconcile returns error, kubernetes tries to call it again 4 more times (5 times total)
     }
 	count++
 	logger.Info("Params", "count", count, "message", msg.Spec.Text)
-	if !msg.ObjectMeta.DeletionTimestamp.IsZero() {
-		msg.SetFinalizers([]string{})
-		if err := r.Update(ctx, &msg); err != nil {
+	if !msg.ObjectMeta.DeletionTimestamp.IsZero() { // deleteTimestamp gets propogated on `kubectl delete ...` run, if finalizers set to be non empty array, just to be able to get resource for the last time in reconcile and then delete it
+		msg.SetFinalizers([]string{}) // clearing finalizers
+		if err := r.Update(ctx, &msg); err != nil { // saving updated value, which clears finalizers, which leads to immidiate and complete resource deletion. Triggers reconcile one more time, but falls into NotFound error and returns None
 			return ctrl.Result{}, err
 		}
 		return ctrl.Result{}, nil
@@ -70,18 +70,18 @@ func (r *MessageReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 
 func (r *MessageReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&Message{}).
+		For(&Message{}). // kind type that will be watched
 		Complete(r)
 }
 
 func main() {
 	scheme := runtime.NewScheme()
-	gvk := schema.GroupVersion{Group: "init.com", Version: "v100"}
-	scheme.AddKnownTypes(gvk, &Message{}, &MessageList{})
+	gvk := schema.GroupVersion{Group: "init.com", Version: "v100"} // new crd group initialization
+	scheme.AddKnownTypes(gvk, &Message{}, &MessageList{}) // new kinds initialization
 	metav1.AddToGroupVersion(scheme, gvk)
 
-	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	ctrl.SetLogger(zap.New(zap.UseDevMode(true))) // another logger can not be used
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{ // new watcher initialization
 		Scheme: scheme,
 	})
 	if err != nil {
@@ -90,13 +90,13 @@ func main() {
 	}
 
 	reconciler := &MessageReconciler{Client: mgr.GetClient()}
-	if err := reconciler.SetupWithManager(mgr); err != nil {
+	if err := reconciler.SetupWithManager(mgr); err != nil { // register created reconcile into new watcher
 		log.Fatalf("unable to set up controller: %v\n", err)
 		os.Exit(1)
 	}
 
-	log.Println("✅ Controller starting...")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+	log.Println("Controller starting...")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil { // start watcher
 		log.Fatalf("problem running manager: %v\n", err)
 		os.Exit(1)
 	}
